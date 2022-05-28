0ffd086b8e5c71364ac528bd0df5ffc73d1eccf5
Ujjwal-varshney <106406227+Ujjwal-varshney@users.noreply.github.com>
2022-05-29 01:43:10 +0530

    Create Merge_two_lists.py

---
+++ Merge_two_lists.py
@@ -0,0 +1,12 @@
+def merge(*lst):
+    lst=[]
+    for i in lst1:
+        lst.append(i)
+    for i in lst2:
+        lst.append(i)
+    print(lst)
+lst1=[]
+lst2=[]
+lst1=list(map(eval,input().split()))
+lst2=list(map(eval,input().split()))
+merge(lst1,lst2)
