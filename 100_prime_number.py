6bdf14916e457609de0a810b40bbfd33bcb5fa3b
Ujjwal-varshney <106406227+Ujjwal-varshney@users.noreply.github.com>
2022-05-29 01:25:48 +0530

    Create 100_prime_number.py

---
+++ 100_prime_number.py
@@ -0,0 +1,12 @@
+i=1
+for x in range(2,1000):
+    flag=0
+    if i>100:
+        break
+    for y in range(2,x):
+        if x%y==0:
+            flag=1
+            break
+    if flag==0:
+        print(x)
+        i=i+1
