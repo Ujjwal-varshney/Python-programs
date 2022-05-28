3dc1506527f675faf4ca21a7f4d0cad3c823188a
Ujjwal-varshney <106406227+Ujjwal-varshney@users.noreply.github.com>
2022-05-29 01:34:27 +0530

    Create Strong_number.py

---
+++ Strong_number.py
@@ -0,0 +1,17 @@
+num=int(input('Enter a number to check whether it is is strong or not: '))
+sum=0
+n=num
+while num>0:
+    a=num%10
+    num=num//10
+    x=a
+    fact=1
+    while x>0:
+    #for x in range(a,0):
+        fact=fact*x
+        x=x-1
+    sum=sum+fact
+if(sum==n):
+    print('It is Strong')
+else:
+    print('It is not strong')
