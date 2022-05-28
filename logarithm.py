d048f4c77bcf1fb2cec806fd7a67f16a80cc7685
Ujjwal-varshney <106406227+Ujjwal-varshney@users.noreply.github.com>
2022-05-29 01:40:08 +0530

    Create logarithm.py

---
+++ logarithm.py
@@ -0,0 +1,7 @@
+import math
+a=float(input("Enter a number: "))
+x=int(input("Enter base of: "))
+print(f'log of {a} with base {x} is: ',math.log(a,x))
+print(f'natural log of {a} is: ',math.log1p(a))
+print(f'log of {a} with base 2 is: ',math.log2(a))
+print(f'log of {a} with base 10 is: ',math.log10(a))
